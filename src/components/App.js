94e18a50ab9de043987ad0ce0f0e0ef2cee01b25
kir-hub <kirill.silin.12321@gmail.com>
2021-03-18 16:53:25 +0200

    made item card and request info for it

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import '../styles/App.css';
+import Item from "./Item";
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>My React App!</h1>
+                <Item/>
             </div>
         );
     }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,29 +1,48 @@
-import React, {useState, useCallback} from 'react'
-import data from './data.json'
+import React, {useState, useCallback, useEffect} from 'react'
+import getMeal from './api'
+import response from './api'
 
 export default function Item(props) {
     const [isFull, setFull] = useState(false)
-    const showCard = useCallback(()=>setFull(!isFull))
-    const displayFull = ()=>{
-        if(isFull){
-            return (
-                <>
-                <h1 onClick={showCard}>title: </h1>
-                <p>meal-type: </p>
-                <p> people it serves: </p>
-                <p>difficulty: </p>
-                <p>ingredients: <ul> </ul> </p>
-                <p>steps: <ul></ul> </p>
-                </>
-            );
-        }else{
-            <h1 onClick={showCard}>title</h1>
-        }
+    const [meal, setMeal] = useState('')
+    
+    const showCard = useCallback(()=>setFull(prevState => !prevState))
+    const fetchMeal= async()=>{
+        const response = await getMeal()
+        const data = response.data.meals[0]
+        console.log(data);
+        setMeal(data)
+        console.log(meal);
     }
+    
+    useEffect(()=>{
+        fetchMeal()
+    },[])
 
+    const ingredient = ()=>{
+        const arr = []
+        for(let i = 1; i <=20; i++){
+            const key = `strIngredient${i}`
+            if(meal[key] !== "" && meal[key] !== null){
+                arr.push(meal[key])
+            }
+        }
+        return arr
+    }
+    const arr = ingredient()
+    
     return (
         <div>
-            {displayFull()}
+            {
+                isFull ?
+                <>
+                <h1 onClick={showCard}>title: {meal.strMeal}</h1>
+                <p>meal-type: {meal.strCategory}</p>
+                <p>ingredients: <ul>{arr.map((item,index)=><li key={index}>{item}</li>)} </ul> </p>
+                <p>steps: {meal.strInstructions}<ul></ul> </p>
+                </> : 
+                <h1 onClick={showCard}>title: {meal.strMeal}</h1>
+            }
         </div>
     )
 }
