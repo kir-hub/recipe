4e1d9e36a1422826944fc03fbc86834e2efbcacf
kir-hub <kirill.silin.12321@gmail.com>
2021-03-19 13:19:29 +0200

    removed .bind and made (id)=>()=>{...}

--- src/components/Item.js
+++ src/components/Item.js
@@ -40,14 +40,14 @@ export default function Item(props) {
         return arr
     })
 
-    const getCard = useCallback((id)=>{
+    const getCard = useCallback((id)=>()=>{
         setInd(id)
         showCard()
     },[])
     
     return (
         <div>
-            <ul>{meal ? meal.map((item,index)=> <li onClick={getCard.bind(index)}>{item.strMeal}</li>) : 'loading...'}</ul>
+            <ul>{meal ? meal.map((item,index)=> <li onClick={getCard(index)}>{item.strMeal}</li>) : 'loading...'}</ul>
             {
                 isFull &&
                 <>
